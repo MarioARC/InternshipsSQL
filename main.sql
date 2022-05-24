2e1a553dae47a1b1117eb4f7da014f1a9f580a89
Mario Castelo <48489226+MarioARC@users.noreply.github.com>
2022-05-24 15:05:20 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,12 @@
+CREATE TABLE internships
+(
+  name TEXT,
+  workhours TEXT,
+  income DOUBLE,
+  location TEXT,
+  requirements TEXT,
+  companytype TEXT,
+  employer TEXT
+);
+
+INSERT INTO internships values
