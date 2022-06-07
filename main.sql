b1da7b560e55d3df21d00e887f63fb5e73469c56
Mario Castelo <48489226+MarioARC@users.noreply.github.com>
2022-06-07 17:21:39 +0000

    added more internships and sorted alphabetically by name

--- main.sql
+++ main.sql
@@ -11,19 +11,20 @@ INSERT INTO internships values
   ('Software Development Internship', 'Internship', '55k-120k', 'Irvine, CA', 'Numecent'),
   ('Software Engineer Intern', 'Full-time', '33k-110k', 'El Segundo, CA', 'Teradata'),
   ('Enterprise Software Engineer', 'Full-time', 'unknown', 'Illinois', 'Wolters Kluwer'),
-  ('Computer Support Analyst', 'Full-time', '53k-83k', 'San Diego, CA', 'Strategic Data Systems');
+  ('Computer Support Analyst', 'Full-time', '53k-83k', 'San Diego, CA', 'Strategic Data Systems'),
+  ('IT Support Specialist', 'Full-time', '33k-65k', 'Tracy, CA', 'Versar Incorporated'),
+  ('Front-End Software Engineer Intern 2022', 'Full-time', 'unknown', 'Sunnyvale, CA', 'Illumio'),
+  ('Internship-Software Developer', 'Full-time', 'unknown', 'Dallas, TX', 'Amadeus'),
+  ('2022 Software Engineer', 'Full-time', '65k-250k', 'Chicago, IL', 'Capgemini'),
+  ('Systems Engineer Specialist, SASE Strategics', 'Full-time', 'unknown', 'Chicago, IL', 'Palo Alto Networks'),
+  ('Associate Software Engineer', 'Full-time', '13k-500k', 'Carlsbad, CA', 'Ricardo plc');
 
 .print
 .print 'Internships'
 .print
   
 SELECT *
-FROM internships;
-
-.print
-
-SELECT *
 FROM internships
-WHERE worktype = 'Internship';
+ORDER BY name;
 
 .print
